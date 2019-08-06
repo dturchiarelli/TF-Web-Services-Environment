5e71842c96de122b52a4595a9a06c88d1a2de5b8
DAZZLER <dturchiarelli@Hotmail.com>
2019-08-06 15:24:45 +1000

    Update main.tf

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 # Configure the Azure Provider
-
 provider "azurerm" {
 # The `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=1.20.0"
